61e1cb86628be8e1e6e37287269b45faa7cde111
Chris Kernaghan <chrisk616@hotmail.co.uk>
2024-10-02 20:32:26 +0100

    Projectile Launch
    Player can now press spacebar to launch projectiles. Both projectiles and animals are being removed from the screen.

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     
     public float horizontalInput;
+    public GameObject projectilePrefab;
     public float speed = 10.0f;
     public float xRange = 10;
 
@@ -30,5 +31,10 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); // Spawn prefeb at players position with prefabs rotation
+        }
     }
 }
